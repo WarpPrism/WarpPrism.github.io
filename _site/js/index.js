f2a06ff424f810e4369bec19f335e3c5e36d147c
ZhouJihao <249662605@qq.com>
2015-12-03 01:46:08 +0800

    update

--- _site/js/index.js
+++ _site/js/index.js
@@ -12,7 +12,7 @@ function scrollAnimation() {
     if (stop != 1) {
         var scrollTop = $(document).scrollTop();
         var columns = $("#index-main-content").find('.content-column');
-        if (scrollTop >= $(columns[0]).offset().top - 400) {
+        if (scrollTop >= $(columns[0]).offset().top - 500) {
             for (var i = 0; i < columns.length; i++) {
                 $(columns[i]).animate({
                     opacity: 1,
