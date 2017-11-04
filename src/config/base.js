981d0afbc91acc6f92c0db2ef02f0bf48bfdaca9
ZhouJihao <249662605@qq.com>
2017-11-04 22:51:12 +0800

    AP

--- src/config/base.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Settings configured here will be merged into the final config object.
-export default {
-    externals: {
-        jquery: 'window.$',
-        zepto: 'window.$',
-    }
-}
